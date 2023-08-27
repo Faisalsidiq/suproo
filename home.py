8f5b33f2b09ea6b8fc149a73ad1c9ece4c32c582
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-27 23:14:51 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-from streamlit_option_menu import option_menu
 
 def main():
-    # URL to the new Google Sheets CSV export link
+    # URL to the Google Sheets CSV export link
     csv_url = "https://docs.google.com/spreadsheets/d/1tjFxtP6AiQ2xZ927yGs1kCB5Cg9OSNeWA-McsX5Bxq8/export?format=csv"
 
     # Load CSV data using pandas
@@ -16,9 +15,19 @@ def main():
     # Display the data
     st.dataframe(df)
 
-    # Create a line chart using "Time" and "O3" columns
+    # Date and time range selection
+    st.write("Select Date and Time Range")
+    min_date = pd.to_datetime(df['Date'].min()).date()
+    max_date = pd.to_datetime(df['Date'].max()).date()
+    selected_start_date = st.date_input("Start Date", min_value=min_date, max_value=max_date, value=min_date)
+    selected_end_date = st.date_input("End Date", min_value=min_date, max_value=max_date, value=max_date)
+    
     st.write("Line Chart: Time vs. O3")
-    line_chart_data = df[["Time", "O3"]]
+    
+    # Filter data based on selected date range
+    filtered_data = df[(df['Date'] >= str(selected_start_date)) & (df['Date'] <= str(selected_end_date))]
+    
+    line_chart_data = filtered_data[["Time", "O3"]]
     st.line_chart(line_chart_data.set_index('Time'))
 
 if __name__ == "__main__":
