c1b3971c8cd5b149efc816a3d3c705940673b28b
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-28 20:43:03 +0700

    Update 1_real-time.py

--- pages/1_real-time.py
+++ pages/1_real-time.py
@@ -16,21 +16,30 @@ latest_row = df.iloc[-1]
 # Create a Folium map centered around the given latitude and longitude
 m = folium.Map(location=[-7.783000, 110.410538], zoom_start=15)
 
-# Add a marker for the given latitude and longitude
+# Define a custom function to create a popup with data values
+def create_popup(row):
+    popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
+    for column in df.columns[1:]:
+        popup += '<b>{}:</b> {}<br>'.format(column, row[column])
+    return folium.Popup(popup, max_width=300)
+
+# Add a marker for the given latitude and longitude with the custom popup
 folium.Marker(
     location=[-7.783000, 110.410538],
-    popup="Click here to load latest value",
+    popup=create_popup(latest_row),
     icon=folium.Icon(color="blue"),
 ).add_to(m)
 
 # Display the map
 folium_static(m)
 
-# When the marker is clicked, display the latest value
-if st.button("Click to Load Latest Value"):
-    st.write("Latest Value:")
-    selected_column = st.selectbox("Select a column", df.columns[1:])
-    st.write(f"{selected_column}: {latest_row[selected_column]}")
-
-
+# When the marker is clicked, display the custom popup
+if m.get_name() in st._widget_to_ctx:
+    marker = st._widget_to_ctx[m.get_name()].map_children[0]
+    if marker.get_name() in st._widget_to_ctx:
+        clicked = st._widget_to_ctx[marker.get_name()].button("Click to Load Latest Value")
+        if clicked:
+            st.write("Latest Value:")
+            selected_column = st.selectbox("Select a column", df.columns[1:])
+            st.write(f"{selected_column}: {latest_row[selected_column]}")
 
