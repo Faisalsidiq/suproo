9e7615fa62e975b3ac73b4bee7f577c8d2f92d5a
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-28 20:20:16 +0700

    Update 1_real-time.py

--- pages/1_real-time.py
+++ pages/1_real-time.py
@@ -13,7 +13,7 @@ df['Date_Time'] = pd.to_datetime(df['Date_Time'])
 latest_row = df.iloc[-1]
 
 # Create a Folium map centered around the given latitude and longitude
-m = folium.Map(location=[-7.783000, 110.410538], zoom_start=15)
+m = folium.Map(location=[-7.783000, 110.410538], zoom_start=15, height=1000, width=1000)
 
 # Define a custom function to create a popup with data values
 def create_popup(row):
@@ -37,6 +37,4 @@ st.title("Map with Latest Data Value")
 st.write("Click the blue marker to view the latest data from the CSV.")
 st.components.v1.html(folium_html)
 
-# Apply custom CSS style to adjust map frame size
-map_html = f'<div style="width: 800px; height: 600px;">{m.get_root().render()}</div>'
-st.markdown(map_html, unsafe_allow_html=True)
+
