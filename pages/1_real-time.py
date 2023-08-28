5ac1dd02a8889e203e84370663aa48207503b8ff
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-28 20:22:55 +0700

    Update 1_real-time.py

--- pages/1_real-time.py
+++ pages/1_real-time.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import folium
-from io import BytesIO
 
 # URL to the Google Sheets CSV export link
 csv_url = "https://docs.google.com/spreadsheets/d/1tjFxtP6AiQ2xZ927yGs1kCB5Cg9OSNeWA-McsX5Bxq8/export?format=csv"
@@ -16,20 +15,26 @@ latest_row = df.iloc[-1]
 # Create a Folium map centered around the given latitude and longitude
 m = folium.Map(location=[-7.783000, 110.410538], zoom_start=15)
 
-# Add a marker for the given latitude and longitude
+# Define a custom function to create a popup with data values
+def create_popup(row):
+    popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
+    for column in df.columns[1:]:
+        popup += '<b>{}:</b> {}<br>'.format(column, row[column])
+    return folium.Popup(popup, max_width=600)
+
+# Add a marker for the given latitude and longitude with the custom popup
 folium.Marker(
     location=[-7.783000, 110.410538],
-    popup="Click here to load latest value",
+    popup=create_popup(latest_row),
     icon=folium.Icon(color="blue"),
 ).add_to(m)
 
-# Convert the Folium map to an image
-image = BytesIO()
-m.save(image, close_file=False)
-
-# Display the map image with a specific size
+# Display the map using Streamlit's map component
 st.title("Map with Latest Data Value")
-st.write("Click the blue marker to load the latest value from the CSV.")
-st.image(image, width=800, height=600)  # Set the width and height here
+st.write("Click the blue marker to view the latest data from the CSV.")
+
+# Apply custom CSS style to adjust map frame size
+map_html = f'<div style="width: 800px; height: 600px;">{m.get_root().render()}</div>'
+st.markdown(map_html, unsafe_allow_html=True)
 
 
