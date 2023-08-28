819df9b35f674d863248b0648aa854bf2c83a042
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-28 20:15:54 +0700

    Update 1_real-time.py

--- pages/1_real-time.py
+++ pages/1_real-time.py
@@ -13,7 +13,13 @@ df['Date_Time'] = pd.to_datetime(df['Date_Time'])
 latest_row = df.iloc[-1]
 
 # Create a Folium map centered around the given latitude and longitude
-m = folium.Map(location=[-7.783000, 110.410538], zoom_start=15)
+m = folium.Map(
+    location=[-7.783000, 110.410538],
+    zoom_start=15,
+    width=800,
+    height=600
+)
+
 
 # Define a custom function to create a popup with data values
 def create_popup(row):
