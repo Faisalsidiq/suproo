80087c3ed31537e8b6e4f91c5a2dfc25f3115e1c
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-29 04:59:27 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 
 def main():
+
+    logo = "logo.png"
+    # Display the logo image
+    st.image(logo_path, width=200)  # Adjust the width as needed
+    
     # URL to the Google Sheets CSV export link
     csv_url = "https://docs.google.com/spreadsheets/d/1tjFxtP6AiQ2xZ927yGs1kCB5Cg9OSNeWA-McsX5Bxq8/export?format=csv"
 
