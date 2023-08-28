3168ce2efa74ea399ef9479432cf53791f39b01d
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-08-28 16:18:50 +0700

    Update 2_analysis.py

--- pages/2_analysis.py
+++ pages/2_analysis.py
@@ -20,22 +20,28 @@ selected_pollutant = st.sidebar.selectbox('Select pollutant column', ['SO2', 'CO
 # Filter data based on user selection
 filtered_df = df[(df['Date_Time'].dt.date == selected_date) & (df['Date_Time'].dt.time == selected_time)]
 
-# Calculate correlation
-correlation = filtered_df[selected_meteorology].astype(float).corr(filtered_df[selected_pollutant].astype(float))
+# Ensure selected_meteorology and selected_pollutant columns contain numeric values
+numeric_cols = ['Temperature', 'Humidity', 'SO2', 'CO', 'O3', 'NO2', 'HC', 'PM1p0', 'PM2p5', 'PM10']
+if selected_meteorology not in numeric_cols or selected_pollutant not in numeric_cols:
+    st.warning("Selected columns should be numeric for correlation calculation.")
+else:
+    # Calculate correlation
+    correlation = filtered_df[selected_meteorology].astype(float).corr(filtered_df[selected_pollutant].astype(float))
 
-# Main content
-st.title('Google Sheets Data Viewer')
-st.write('Selected Data:')
-st.write(f'Date: {selected_date}, Time: {selected_time}')
-st.write(f'Meteorology: {selected_meteorology}')
-st.write(f'Pollutant: {selected_pollutant}')
-st.write(filtered_df[[selected_meteorology, selected_pollutant]])
+    # Main content
+    st.title('Google Sheets Data Viewer')
+    st.write('Selected Data:')
+    st.write(f'Date: {selected_date}, Time: {selected_time}')
+    st.write(f'Meteorology: {selected_meteorology}')
+    st.write(f'Pollutant: {selected_pollutant}')
+    st.write(filtered_df[[selected_meteorology, selected_pollutant]])
 
-# Correlation line plot
-st.write('Correlation Line Plot:')
-plt.figure(figsize=(8, 6))
-sns.regplot(x=filtered_df[selected_meteorology].astype(float), y=filtered_df[selected_pollutant].astype(float))
-plt.xlabel(selected_meteorology)
-plt.ylabel(selected_pollutant)
-plt.title(f'Correlation: {correlation:.2f}')
-st.pyplot(plt)
+    # Correlation line plot
+    st.write('Correlation Line Plot:')
+    plt.figure(figsize=(8, 6))
+    sns.regplot(x=filtered_df[selected_meteorology].astype(float), y=filtered_df[selected_pollutant].astype(float))
+    plt.xlabel(selected_meteorology)
+    plt.ylabel(selected_pollutant)
+    plt.title(f'Correlation: {correlation:.2f}')
+    st.pyplot(plt)
+
