ee555ff6e05504cd2512f54bef79a392b8ae1353
Faisalsidiq <88192743+Faisalsidiq@users.noreply.github.com>
2023-09-12 04:36:08 +0700

    Update 2_📊_analysis.py

--- pages/2_📊_analysis.py
+++ pages/2_📊_analysis.py
@@ -4,8 +4,7 @@ from datetime import datetime, time
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import random
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
+import urllib.parse
 
 # Function to preprocess data
 def preprocess_value(value):
@@ -88,41 +87,17 @@ if selected_tool == 'Correlation':
             correlation_coefficient = correlation_df.iloc[0, 1]
             # Display the correlation coefficient
             st.write("Correlation Coefficient:", correlation_coefficient)
+            hasil_so2 = st.session_state.hasil_so2
+
+# Menampilkan hasil pelatihan model SO2
+st.write("Hasil Pelatihan Model SO2:", hasil_so2)
         else:
             st.write("Not enough data for correlation calculation.")
     else:
         st.write("No data available for correlation calculation.")
 
-        # Menambahkan kolom Datetime
-    df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
-    df = df.drop(['Date', 'Time'], axis=1)
-    df.set_index('Datetime', inplace=True)
-    
-    
-    st.title("Prediksi CO dengan Model ARIMA")
-    
-    # CO predictions
-    df['CO'] = pd.to_numeric(df['CO'], errors='coerce', downcast='integer')
-    CO = df['CO']
-    
-    train_size = int(len(CO) * 0.8)
-    train, test = CO[:train_size], CO[train_size:]
-    
-    p, d, q = 5, 1, 0
-    model = ARIMA(train, order=(p, d, q))
-    model_fit = model.fit()
-    
-    # Predict CO values for the next 7 days
-    predictions = model_fit.forecast(steps=7)
-    
-    # Create date range for predictions
-    prediction_dates = pd.date_range(start=CO.index[train_size], periods=len(predictions), freq='D')
-    
-    # Print CO predictions for each day
-    st.subheader("Prediksi CO untuk 7 hari ke depan:")
-    for date, prediction in zip(prediction_dates, predictions):
-        st.write(f'Tanggal: {date.date()}, Prediksi CO: {prediction:.2f}')
-
+     
+ 
 elif selected_tool == 'Statistics':
     st.write('Statistics tool selected')
     
