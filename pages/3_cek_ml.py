38c3da7776ce525c7d7d8ffee7ef7696f64371b8
Faisalsidiq <88192743+Faisalsidiq@users.noreply.github.com>
2023-09-12 04:18:56 +0700

    Update 3_cek_ml.py

--- pages/3_cek_ml.py
+++ pages/3_cek_ml.py
@@ -14,26 +14,26 @@ df = df.drop(['Date', 'Time'], axis=1)
 df.set_index('Datetime', inplace=True)
 
 
-st.title("Prediksi CO dengan Model ARIMA")
+st.title("Prediksi SO2 dengan Model ARIMA")
 
 # CO predictions
-df['CO'] = pd.to_numeric(df['CO'], errors='coerce', downcast='integer')
-CO = df['CO']
+df['SO2'] = pd.to_numeric(df['SO2'], errors='coerce', downcast='integer')
+SO2 = df['SO2']
 
-train_size = int(len(CO) * 0.8)
-train, test = CO[:train_size], CO[train_size:]
+train_size = int(len(SO2) * 0.8)
+train, test = SO2[:train_size], SO2[train_size:]
 
 p, d, q = 5, 1, 0
 model = ARIMA(train, order=(p, d, q))
 model_fit = model.fit()
 
-# Predict CO values for the next 7 days
+# Predict SO2 values for the next 7 days
 predictions = model_fit.forecast(steps=7)
 
 # Create date range for predictions
-prediction_dates = pd.date_range(start=CO.index[train_size], periods=len(predictions), freq='D')
+prediction_dates = pd.date_range(start=SO2.index[train_size], periods=len(predictions), freq='D')
 
-# Print CO predictions for each day
-st.subheader("Prediksi CO untuk 7 hari ke depan:")
+# Print SO2 predictions for each day
+st.subheader("Prediksi SO2 untuk 7 hari ke depan:")
 for date, prediction in zip(prediction_dates, predictions):
-    st.write(f'Tanggal: {date.date()}, Prediksi CO: {prediction:.2f}')
+    st.write(f'Tanggal: {date.date()}, Prediksi SO2: {prediction:.2f}')
