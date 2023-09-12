b1a303f4ef778ac1ce74bb09e48455ed5cc16551
Faisalsidiq <88192743+Faisalsidiq@users.noreply.github.com>
2023-09-12 21:58:40 +0700

    Update 1_⏰_real-time.py

--- pages/1_⏰_real-time.py
+++ pages/1_⏰_real-time.py
@@ -29,109 +29,29 @@ latest_row_second = df_second.iloc[-1]
 # Create a Folium map centered around a specific latitude and longitude
 m = folium.Map(location=[-7.786335507018436, 110.38799288469626], zoom_start=15)
 
-def hitung_tingkat_polusi(pollutant_name, value):
-    if pollutant_name == 'CO':
-        if value < 4000:
-            return "Baik"
-        elif value >= 4000 and value < 8000:
-            return "Sedang"
-        elif value >= 8000 and value < 30000:
-            return "Tidak Sehat"
-        elif value >= 30000 and value < 45000:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'SO2':
-        if value < 52:
-            return "Baik"
-        elif value >= 52 and value < 180:
-            return "Sedang"
-        elif value >= 180 and value < 400:
-            return "Tidak Sehat"
-        elif value >= 400 and value < 800:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'O3':
-        if value < 120:
-            return "Baik"
-        elif value >= 120 and value < 235:
-            return "Sedang"
-        elif value >= 235 and value < 400:
-            return "Tidak Sehat"
-        elif value >= 400 and value < 800:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'NO2':
-        if value < 80:
-            return "Baik"
-        elif value >= 80 and value < 200:
-            return "Sedang"
-        elif value >= 200 and value < 1130:
-            return "Tidak Sehat"
-        elif value >= 1130 and value < 2260:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'HC':
-        if value < 45:
-            return "Baik"
-        elif value >= 45 and value < 1000:
-            return "Sedang"
-        elif value >= 1000 and value < 215:
-            return "Tidak Sehat"
-        elif value >= 215 and value < 432:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'PM10':
-        if value < 50:
-            return "Baik"
-        elif value >= 50 and value < 150:
-            return "Sedang"
-        elif value >= 150 and value < 350:
-            return "Tidak Sehat"
-        elif value >= 350 and value < 420:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    elif pollutant_name == 'PM2p5':
-        if value < 15.5:
-            return "Baik"
-        elif value >= 15.5 and value < 55.4:
-            return "Sedang"
-        elif value >= 55.4 and value < 150.4:
-            return "Tidak Sehat"
-        elif value >= 150.4 and value < 250.4:
-            return "Sangat Tidak Sehat"
-        else:
-            return "Berbahaya"
-    else:
-        return "Tidak Diketahui"
-
-
 # Define custom functions to create popups with data values
 def create_popup_first(row):
     popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
-    for column in df_first.columns[1:]:
-        df_first[column] = df_first[column].astype(float) 
-        pollutant_name = column
-        value = row[column]
-        category = hitung_tingkat_polusi(pollutant_name, value)
-        popup += '<b>{}:</b> {} ({})<br>'.format(pollutant_name, value, category)
-
+    popup += '<b>CO:</b> {} ({})<br>'.format(row['CO'], hitung_tingkat_polusi('CO', row['CO']))
+    popup += '<b>SO2:</b> {} ({})<br>'.format(row['SO2'], hitung_tingkat_polusi('SO2', row['SO2']))
+    popup += '<b>O3:</b> {} ({})<br>'.format(row['O3'], hitung_tingkat_polusi('O3', row['O3']))
+    popup += '<b>NO2:</b> {} ({})<br>'.format(row['NO2'], hitung_tingkat_polusi('NO2', row['NO2']))
+    popup += '<b>HC:</b> {} ({})<br>'.format(row['HC'], hitung_tingkat_polusi('HC', row['HC']))
+    popup += '<b>PM10:</b> {} ({})<br>'.format(row['PM10'], hitung_tingkat_polusi('PM10', row['PM10']))
+    popup += '<b>PM2p5:</b> {} ({})<br>'.format(row['PM2p5'], hitung_tingkat_polusi('PM2p5', row['PM2p5']))
     return folium.Popup(popup, max_width=300)
 
 def create_popup_second(row):
     popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
-    for column in df_second.columns[1:]:
-        pollutant_name = column
-        value = row[column]
-        category = hitung_tingkat_polusi(pollutant_name, value)
-        popup += '<b>{}:</b> {} ({})<br>'.format(pollutant_name, value, category)
-
+    popup += '<b>CO:</b> {} ({})<br>'.format(row['CO'], hitung_tingkat_polusi('CO', row['CO']))
+    popup += '<b>SO2:</b> {} ({})<br>'.format(row['SO2'], hitung_tingkat_polusi('SO2', row['SO2']))
+    popup += '<b>O3:</b> {} ({})<br>'.format(row['O3'], hitung_tingkat_polusi('O3', row['O3']))
+    popup += '<b>NO2:</b> {} ({})<br>'.format(row['NO2'], hitung_tingkat_polusi('NO2', row['NO2']))
+    popup += '<b>HC:</b> {} ({})<br>'.format(row['HC'], hitung_tingkat_polusi('HC', row['HC']))
+    popup += '<b>PM10:</b> {} ({})<br>'.format(row['PM10'], hitung_tingkat_polusi('PM10', row['PM10']))
+    popup += '<b>PM2p5:</b> {} ({})<br>'.format(row['PM2p5'], hitung_tingkat_polusi('PM2p5', row['PM2p5']))
     return folium.Popup(popup, max_width=300)
+
 
 # Add markers for the first and second latitude and longitude with custom popups
 folium.Marker(
