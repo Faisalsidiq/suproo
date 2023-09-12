cd4f93742d17e4e5cec4d316b29914f02140ce74
Faisalsidiq <88192743+Faisalsidiq@users.noreply.github.com>
2023-09-12 22:22:18 +0700

    Update 1_⏰_real-time.py

--- pages/1_⏰_real-time.py
+++ pages/1_⏰_real-time.py
@@ -115,6 +115,13 @@ def hitung_tingkat_polusi(pollutant_name, value):
             return "Berbahaya"
 
 
+def create_popup_first(row):
+    popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
+    for column in df_first.columns[1:]:
+        value = row[column]
+        pollutant_category = hitung_tingkat_polusi(column, value)
+        popup += '<b>{}:</b> {} ({})<br>'.format(column, value, pollutant_category)
+    return folium.Popup(popup, max_width=300)
 
 def create_popup_second(row):
     popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
@@ -124,13 +131,7 @@ def create_popup_second(row):
         popup += '<b>{}:</b> {} ({})<br>'.format(column, value, pollutant_category)
     return folium.Popup(popup, max_width=300)
 
-def create_popup_first(row):
-    popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
-    for column in df_first.columns[1:]:
-        value = row[column]
-        pollutant_category = hitung_tingkat_polusi(column, value)
-        popup += '<b>{}:</b> {} ({})<br>'.format(column, value, pollutant_category)
-    return folium.Popup(popup, max_width=300)
+
 
 
 
