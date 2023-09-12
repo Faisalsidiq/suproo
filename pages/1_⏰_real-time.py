91d3b6eb59aac0120f0a43b98b9079a1c477b45f
Faisalsidiq <88192743+Faisalsidiq@users.noreply.github.com>
2023-09-12 21:44:02 +0700

    Update 1_⏰_real-time.py

--- pages/1_⏰_real-time.py
+++ pages/1_⏰_real-time.py
@@ -29,6 +29,88 @@ latest_row_second = df_second.iloc[-1]
 # Create a Folium map centered around a specific latitude and longitude
 m = folium.Map(location=[-7.786335507018436, 110.38799288469626], zoom_start=15)
 
+def hitung_tingkat_polusi(pollutant_name, value):
+    if pollutant_name == 'CO':
+        if value < 4000:
+            return "Baik"
+        elif value >= 4000 and value < 8000:
+            return "Sedang"
+        elif value >= 8000 and value < 30000:
+            return "Tidak Sehat"
+        elif value >= 30000 and value < 45000:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'SO2':
+        if value < 52:
+            return "Baik"
+        elif value >= 52 and value < 180:
+            return "Sedang"
+        elif value >= 180 and value < 400:
+            return "Tidak Sehat"
+        elif value >= 400 and value < 800:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'O3':
+        if value < 120:
+            return "Baik"
+        elif value >= 120 and value < 235:
+            return "Sedang"
+        elif value >= 235 and value < 400:
+            return "Tidak Sehat"
+        elif value >= 400 and value < 800:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'NO2':
+        if value < 80:
+            return "Baik"
+        elif value >= 80 and value < 200:
+            return "Sedang"
+        elif value >= 200 and value < 1130:
+            return "Tidak Sehat"
+        elif value >= 1130 and value < 2260:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'HC':
+        if value < 45:
+            return "Baik"
+        elif value >= 45 and value < 1000:
+            return "Sedang"
+        elif value >= 1000 and value < 215:
+            return "Tidak Sehat"
+        elif value >= 215 and value < 432:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'PM10':
+        if value < 50:
+            return "Baik"
+        elif value >= 50 and value < 150:
+            return "Sedang"
+        elif value >= 150 and value < 350:
+            return "Tidak Sehat"
+        elif value >= 350 and value < 420:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    elif pollutant_name == 'PM2p5':
+        if value < 15.5:
+            return "Baik"
+        elif value >= 15.5 and value < 55.4:
+            return "Sedang"
+        elif value >= 55.4 and value < 150.4:
+            return "Tidak Sehat"
+        elif value >= 150.4 and value < 250.4:
+            return "Sangat Tidak Sehat"
+        else:
+            return "Berbahaya"
+    else:
+        return "Tidak Diketahui"
+
+
 # Define custom functions to create popups with data values
 def create_popup_first(row):
     popup = '<b>Date:</b> {}<br>'.format(row['Date_Time'])
